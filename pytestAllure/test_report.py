ae25cebeeb9b56094d06aa342cd9376782be71cf
Nikhil Shukla <74907178+nikhil-shukla@users.noreply.github.com>
2023-12-15 17:23:44 +0530

    Update test_report.py

--- pytestAllure/test_report.py
+++ pytestAllure/test_report.py
@@ -6,7 +6,7 @@ def test_value1():
     assert func(2) == 3
     
 def test_value2():
-    assert func(3) == 2
+    assert func(3) == 4
     
 def test_value3():
-    assert func(3) == 4
+    assert func(4) == 5
